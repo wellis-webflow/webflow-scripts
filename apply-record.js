068e4099df248d95431d71db4bd922892d6a85e8
Aleksander Afanasyev <afonia.marcii@gmail.com>
2024-12-18 15:29:20 +0100

    Add function to append record ID to continue button URL on script load

---
+++ apply-record.js
@@ -0,0 +1,14 @@
+function AddRecordIdToUrl() {
+  let urlParams = new URLSearchParams(window.location.search);
+  let identifierValue = urlParams.get('identifier');
+  if (identifierValue) {
+    let continueButton = document.getElementById('continueButton');
+    if (continueButton) {
+      let buttonUrl = new URL(continueButton.href);
+      buttonUrl.searchParams.set(`${encodeURIComponent('extra_fields[record_id]') + '='}`, identifierValue);
+      continueButton.href = buttonUrl.toString();
+    }
+  }
+}
+
+AddRecordIdToUrl();
