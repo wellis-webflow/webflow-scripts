dc1eb01bd283ed3b37f835ad7e2a729149064ede
Aleksander Afanasyev <afonia.marcii@gmail.com>
2024-12-19 10:52:04 +0100

    add new V

---
+++ apply-v2.js
@@ -0,0 +1,15 @@
+function AddRecordId() {
+  let urlParams = new URLSearchParams(window.location.search);
+  let identifierValue = urlParams.get('identifier');
+  if (identifierValue) {
+    let continueButton = document.getElementById('continueButton');
+    if (continueButton) {
+      let buttonUrl = new URL(continueButton.href);
+      console.log('buttonUrl', buttonUrl);
+      buttonUrl.searchParams.set(`${encodeURIComponent('extra_fields[record_id]') + '='}`, identifierValue);
+      console.log('buttonUrlNew', buttonUrl);
+      continueButton.href = buttonUrl.toString();
+      console.log('continueButton', continueButton.href);
+    }
+  }
+}
